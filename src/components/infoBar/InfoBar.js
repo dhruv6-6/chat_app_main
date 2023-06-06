cd8b019c052d899b6532615511ca7eda184f01f8
dhruv6-6 <94751267+dhruv6-6@users.noreply.github.com>
2023-06-06 13:12:49 +0530

    Update InfoBar.js

--- src/components/infoBar/InfoBar.js
+++ src/components/infoBar/InfoBar.js
@@ -10,7 +10,7 @@ const InfoBar = ({room}) =>{
     <div className="infoBar">
         <div className="leftInnerContainer">
             <img className="onlineIcon" src={onlineIcon} alt = "online" />
-            <h1 className="roomShow">test</h1>
+            <h1 className="roomShow">{room}</h1>
         </div>
         <div className="rightInnerContainer">
             <a href = "/"><img src={closeIcon} alt="close"/></a>
